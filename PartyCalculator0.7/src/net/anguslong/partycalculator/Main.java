60113853462b0f97ab347428505bac09ad6e87e1
Angus Long <mail@anguslong.net>
2013-10-11 12:14:14 +0200

    ok working now

--- PartyCalculator0.7/src/net/anguslong/partycalculator/Main.java
+++ PartyCalculator0.7/src/net/anguslong/partycalculator/Main.java
@@ -11,8 +11,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
