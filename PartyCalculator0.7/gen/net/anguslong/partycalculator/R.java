479310f7cdcb22c5047278d0d382592d5c957eb1
Angus Long <mail@anguslong.net>
2013-10-11 11:20:42 +0200

    added button for party type

--- PartyCalculator0.7/gen/net/anguslong/partycalculator/R.java
+++ PartyCalculator0.7/gen/net/anguslong/partycalculator/R.java
@@ -29,28 +29,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int beerResultTextView=0x7f0a000a;
-        public static final int calculateButton=0x7f0a000f;
-        public static final int femaleGuestsEditText=0x7f0a0008;
-        public static final int maleGuestsEditText=0x7f0a0005;
-        public static final int partyLengthEditText=0x7f0a000c;
-        public static final int partyLengthSeekBar=0x7f0a000b;
-        public static final int redWineResultTextView=0x7f0a0010;
-        public static final int sel_party_type=0x7f0a0013;
-        public static final int sel_reset=0x7f0a0014;
-        public static final int sel_settings=0x7f0a0015;
-        public static final int tableRow1=0x7f0a0003;
-        public static final int tableRow2=0x7f0a0006;
-        public static final int tableRow3=0x7f0a0009;
+        public static final int beerResultTextView=0x7f0a000c;
+        public static final int buttonPartyType=0x7f0a0003;
+        public static final int calculateButton=0x7f0a0011;
+        public static final int femaleGuestsEditText=0x7f0a000a;
+        public static final int maleGuestsEditText=0x7f0a0007;
+        public static final int partyLengthEditText=0x7f0a000e;
+        public static final int partyLengthSeekBar=0x7f0a000d;
+        public static final int redWineResultTextView=0x7f0a0012;
+        public static final int sel_party_type=0x7f0a0014;
+        public static final int sel_reset=0x7f0a0015;
+        public static final int sel_settings=0x7f0a0016;
+        public static final int tableRow1=0x7f0a0005;
+        public static final int tableRow2=0x7f0a0008;
+        public static final int tableRow3=0x7f0a000b;
         public static final int tableRow4=0x7f0a0000;
-        public static final int tableRow5=0x7f0a0002;
-        public static final int tableRow6=0x7f0a000e;
-        public static final int textView1=0x7f0a0004;
-        public static final int textView2=0x7f0a0007;
+        public static final int tableRow5=0x7f0a0004;
+        public static final int tableRow6=0x7f0a0010;
+        public static final int textView1=0x7f0a0006;
+        public static final int textView2=0x7f0a0009;
         public static final int textView3=0x7f0a0001;
-        public static final int textView4=0x7f0a000d;
-        public static final int textView5=0x7f0a0011;
-        public static final int whiteWineResultTextView=0x7f0a0012;
+        public static final int textView4=0x7f0a000f;
+        public static final int textView5=0x7f0a0002;
+        public static final int whiteWineResultTextView=0x7f0a0013;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -65,16 +66,18 @@ public final class R {
         public static final int btnCalculate=0x7f060007;
         public static final int choices_title=0x7f06000c;
         public static final int hello_world=0x7f060003;
-        public static final int intensity_pref_key=0x7f060010;
-        public static final int intensity_summary=0x7f06000f;
+        public static final int intensity_pref_key=0x7f060012;
+        public static final int intensity_summary=0x7f060011;
         public static final int intro_text=0x7f06000a;
         public static final int lblFemaleGuests=0x7f060005;
         public static final int lblHours=0x7f060008;
         public static final int lblMaleGuests=0x7f060004;
         public static final int lblPartyLen=0x7f060006;
-        public static final int lbl_intensity_pref=0x7f06000e;
-        public static final int lbl_reset=0x7f06000d;
+        public static final int lbl_intensity_pref=0x7f060010;
+        public static final int lbl_reset=0x7f06000f;
         public static final int lbl_settings=0x7f060002;
+        public static final int party_type_button_text=0x7f06000e;
+        public static final int party_type_text=0x7f06000d;
         public static final int sel_party_type=0x7f06000b;
         public static final int title_activity_party_calculator_result=0x7f060009;
     }
